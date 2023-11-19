fe5a6c773216e0631fcd4a1b370203c03a548f11
Kaleem Ullah <malikkaleemullah2k@gmail.com>
2023-11-20 00:37:32 +0500

    working on

--- script.mjs
+++ script.mjs
@@ -1,10 +1,19 @@
 function toggleDetails(element) {
-    var details = element.nextElementSibling;
-    if (details.style.display === "block") {
-        details.style.display = "none";
-        element.innerHTML = "Show Details";
-    } else {
+    let details = element.nextElementSibling;
+
+    // Check if the cursor entered the element
+    element.addEventListener('mouseenter', function() {
+        // Set the display property to "block" to show the details
         details.style.display = "block";
-        element.innerHTML = "Hide Details";
-    }
-    }
+        // Change the inner HTML of the passed element to show "Hide Details"
+        element.innerHTML = "Course details _";
+    });
+
+    // Check if the cursor left the element
+    element.addEventListener('mouseleave', function() {
+        // Set the display property to "none" to hide the details
+        details.style.display = "none";
+        // Change the inner HTML of the passed element to show "Toggle Details"
+        element.innerHTML = "&#9776; Toggle Details";
+    });
+}
